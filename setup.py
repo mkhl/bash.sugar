af70e2652b00d8f63b020287c442651f9d7dcf3d
Martin Kuehl <martin.kuehl@gmail.com>
2009-05-23 00:15:55 +0200

    Add setup.py script for building the sugar.
    Many thanks to Ian Beck (onecrayon) for having ported the example cocoa sugar
    to python!

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import py2app
+import os
+
+def data_files(dirname = '.'):
+	"""Figure out the sugar structure and massage it into data_files format."""
+	def data_file(name):
+		return (os.path.join('..', '..', name),
+		        [os.path.join(dirname, name, file)
+		         for file in os.listdir(os.path.join(dirname, name))
+		         if not file.startswith('.')])
+	
+	base = ['LICENSE', 'README.mdown', 'Languages.xml']
+	result = [(os.path.join('..', '..'), base)]
+	dirs = set([
+		'CodeSenseLibraries',
+		'CodeSenseProviders',
+		'ContextualSettings',
+		'FileActions',
+		'Itemizers',
+		'PlaceholderThemes',
+		'Syntaxes',
+		'SyntaxInjections',
+		'TextActions',
+	])
+	for name in dirs.intersection(set(os.listdir(dirname))):
+		result.append(data_file(name))
+	return result
+
+setup(
+	name = 'Bash',
+	version = '0.0',
+	author = 'Martin Kühl',
+	author_email = 'martin.kuehl@gmail.com',
+	url = 'http://github.com/mkhl/bash.sugar',
+	description = 'A Sugar adding sweet Bash support for Espresso',
+	data_files = data_files('xml'),
+	plugin = ['src/Itemizers.py'],
+	options = {
+		'py2app': {
+			'extension': '.sugar',
+			'semi_standalone': True,
+		}
+	}
+)
